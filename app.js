ffff60c260f4d90331bb640022da3b3a6bce9f41
Jonathan Svensson <racingjss@hotmail.com>
2022-07-19 07:28:22 -0400

    all nighter go brrr

--- app.js
+++ app.js
@@ -8,7 +8,9 @@
 
 // Imports
 require("dotenv").config();
+const fs = require("fs");
 const express = require("express");
+const { render } = require("ejs");
 const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const bcrypt = require("bcrypt");
@@ -16,18 +18,44 @@ const passport = require("passport");
 const session = require("express-session");
 const flash = require("express-flash");
 const port = process.env.PORT || 8080;
+const initializePassport = require("./auth/passport-config");
 
-const initializePassport = require("./passport-config");
-initializePassport(
-  passport,
-  (email) => users.find((user) => user.email === email),
-  (id) => users.find((user) => user.id === id)
-);
-
-//FIXME: const indexRouter = require('./routes/index')
-
-//FIXME: Update to use a database instead
-const users = [];
+// Only use database authentication if set to true
+const users = [
+  {
+    id: 1,
+    name: "Manager",
+    email: "manager@grocerystore.com",
+    password: "$2b$10$2dhIGmTfMxZw0i8JQ6HJcufcW9wkIt8aIphkK7rn0d0A/718z5ZFO",
+  },
+];
+if (process.env.USE_DB_AUTH == 1) {
+  initializePassport(
+    passport,
+    async (email) => {
+      const result = await db.query("SELECT * FROM users WHERE email = $1", [
+        email,
+      ]);
+      return result.rows[0];
+    },
+    async (id) => {
+      const result = await db.query("SELECT * FROM users WHERE id = $1", [id]);
+      return result.rows[0];
+    }
+  );
+} else {
+  initializePassport(
+    passport,
+    (email) => {
+      const user = users.find((user) => user.email === email);
+      console.log(user);
+      return user;
+    },
+    (id) => {
+      return users.find((user) => user.id === id);
+    }
+  );
+}
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
@@ -48,7 +76,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-//FIXME: app.use('/', indexRouter)
+//
+// Routes
+//
 
 app.get("/", checkAuthenticated, (req, res) => {
   res.render("index", { name: req.user.name });
@@ -77,12 +107,20 @@ app.post(
 app.post("/register", checkNotAuthenticated, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      email: req.body.email,
-      password: hashedPassword,
-    });
+    const id = await Date.now();
+    if (process.env.USE_DB_AUTH == 1) {
+      db.query(
+        "INSERT INTO users(id, name, email, password) VALUES($1, $2, $3, $4)",
+        [id, req.body.name, req.body.email, hashedPassword]
+      );
+    } else {
+      users.push({
+        id: id.toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+      });
+    }
     res.redirect("/login");
   } catch {
     res.redirect("/register");
@@ -98,8 +136,7 @@ app.post("/logout", function (req, res) {
   });
 });
 
-//FIXME:
-app.get("/search", async (req, res) => {
+app.get("/search", checkAuthenticated, async (req, res) => {
   if (req.query.q) {
     const result = await db.query(
       "SELECT * FROM products WHERE name ILIKE $1 OR brand ILIKE $1",
@@ -115,9 +152,57 @@ app.get("/search", async (req, res) => {
   }
 });
 
-//FIXME:
-app.get("/users", (req, res) => {
-  res.json(users);
+// Employees route
+app.get("/employee", checkEmployeeAuthenticated, async (req, res) => {
+  if (req.query.q) {
+    const result = await db.query(
+      "SELECT * FROM products WHERE name ILIKE $1 OR brand ILIKE $1",
+      ["%" + req.query.q + "%"]
+    );
+    console.log("/employee/search: results for '" + req.query.q + "':");
+    console.log(result.rows);
+    console.log();
+    res.render("employee", { name: req.user.name, data: result.rows, q: req.query.q });
+  } else {
+    console.log("/employee/search: No query provided\n");
+    res.render("employee", { name: req.user.name, q: req.query.q });
+  }
+});
+
+app.get('/employee/newproduct', async (req, res) => {
+  res.render("employee/newproduct", {response: null})
+})
+
+app.get('/employee/updateproduct', async (req, res) => {
+  res.render("employee/updateproduct")
+})
+
+app.post('/employee/newproduct', async (req, res) => {
+    await db.query(
+      "INSERT INTO products(upc, name, brand, price, qty) VALUES($1, $2, $3, $4, $5)",
+      [req.body.upc, req.body.name, req.body.brand, req.body.price, req.body.qty]
+    );
+    res.redirect("/employee/newproduct");
+})
+
+// New Employee route
+app.get("employee/new", (req, res) => {
+  res.render("employee/new");
+});
+
+// Create new employee route
+app.post("/", (req, res) => {
+  res.send("Create");
+});
+
+// FIXME: debugging
+app.get("/users", async (req, res) => {
+  if (process.env.USE_DB_AUTH == 1) {
+    const results = await db.query("SELECT * FROM users");
+    res.send(results.rows);
+  } else {
+    res.json(users);
+  }
 });
 
 function checkAuthenticated(req, res, next) {
@@ -135,16 +220,33 @@ function checkNotAuthenticated(req, res, next) {
   next();
 }
 
-// PostgreSQL
-const db = require("./db");
-const { render } = require("ejs");
+async function checkEmployeeAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    const results = await db.query(
+      "SELECT EXISTS (SELECT id FROM employees WHERE id = $1) AS isEmployee",
+      [req.user.id]
+    );
+    if (results.rows[0]) {
+      console.log("Employee authenticated successfully");
+      return next();
+    }
+  }
+  console.log("Employee authentication failed");
+  res.redirect("/login");
+}
 
-// Verifies the connection to the postgres database and returns a SELECT NOW() query
-db.connect((err, client, release) => {
+//
+// PostgreSQL
+//
+
+const db = require("./db");
+
+// Verifies the connection to the postgres database and initializes it
+db.connect(async (err, client, release) => {
   if (err) {
     return console.error("Error acquiring client", err.stack);
   }
-  client.query(
+  await client.query(
     "SELECT $1::text as connected",
     ["Connection to postgres successful!"],
     (err, result) => {
@@ -159,36 +261,54 @@ db.connect((err, client, release) => {
         }
         console.log(res.rows[0]);
         console.log();
+
         release();
       });
     }
   );
-});
-
-/* const { Client } = require('pg');
-
-(async () => {
-  const client = new Client();
-  await client.connect();
-  const res = await client.query('SELECT $1::text as connected', ['Connection to postgres successful!']);
-  console.log(res.rows[0].connected);
-  await client.end();
-})();
-
-db.query('SELECT NOW()', (err, res) => {
-    if (err) {
-        console.log(err.stack)
-    } else {
-        console.log(res.rows[0])
+  if (process.env.INIT_DB == 1) {
+    if (fs.existsSync("products.csv")) {
+      await console.log("DB: Initializing products");
+      await client.query("DROP TABLE IF EXISTS products");
+      await client.query(
+        "CREATE TABLE products (upc char(12) PRIMARY KEY, name varchar(50), brand varchar(30), price numeric(7, 2), qty int)"
+      );
+      await console.log("DB: Re-created products table");
+      await client.query(
+        "COPY products(upc, name, brand, price, qty) FROM '" +
+          __dirname +
+          "/products.csv' DELIMITER ',' CSV HEADER"
+      );
+      await console.log("DB: initialized products table from products.csv");
     }
+    if (fs.existsSync("managers.csv")) {
+      await client.query("DROP TABLE IF EXISTS users CASCADE");
+      await client.query("DROP TABLE IF EXISTS employees");
+      await client.query("DROP TABLE IF EXISTS managers");
+      await client.query(
+        "CREATE TABLE users (id BIGINT PRIMARY KEY, name TEXT, email TEXT, password TEXT)"
+      );
+      await client.query(
+        "CREATE TABLE managers (id BIGINT PRIMARY KEY, CONSTRAINT id_fk FOREIGN KEY(id) REFERENCES users(id));"
+      );
+      await client.query(
+        "CREATE TABLE employees (id BIGINT PRIMARY KEY, CONSTRAINT id_fk FOREIGN KEY(id) REFERENCES users(id));"
+      );
+      await console.log("DB: Re-created user tables");
+      await client.query(
+        "COPY users(id, name, email, password) FROM '" +
+          __dirname +
+          "/managers.csv' DELIMITER ',' CSV HEADER"
+      );
+      await client.query("INSERT INTO employees(id) SELECT id FROM users");
+      await client.query("INSERT INTO managers(id) SELECT id FROM users");
+      await console.log("DB: initialized user tables");
+      await console.log("DB: Done!");
+    }
+  } else {
+    console.log("DB: Skipping initialization");
+  }
 });
- */
-
-/*
-app.get('/', (req, res) => {
-  // res.send('Hello World!')
-})
-*/
 
 // Start listening to requests on the set port
 app.listen(port, () => {
